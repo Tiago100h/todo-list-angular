7d355dea95ced32d9cd587303bb3684279397e85
Tiago100h <tiagogiuliatte.p@gmail.com>
2024-06-17 15:14:58 -0300

    feat: implement error interceptor

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -1,21 +1,23 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { catchError, throwError } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
 
   const matSnackBar = inject(MatSnackBar);
 
-  // next(req).subscribe({
-  //   error: (err) => {
-  //     const errorMessage = `⚠️ ${err?.message || 'Unknown Error'}`;
+  return next(req).pipe(
+    catchError((err) => {
+      const errorMessage = `⚠️ ${err?.message || 'Unknown Error'}`;
 
-  //     matSnackBar.open(errorMessage, 'Close', { 
-  //       duration: 5000, 
-  //       horizontalPosition: 'end' 
-  //     });
-  //   }
-  // });
-
-  return next(req);
+      matSnackBar.open(errorMessage, 'Close', { 
+        duration: 5000, 
+        horizontalPosition: 'end' 
+      });
+      
+      return throwError(() => new Error(err))
+    })
+  );
+  
 };
